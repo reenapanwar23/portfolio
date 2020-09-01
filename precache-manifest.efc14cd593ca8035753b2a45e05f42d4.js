4cad4c3d2ca73967f404acfaeb8c0416c22e23f4
Reena Panwar <reenapanwar.23@gmail.com>
2020-09-01 15:29:49 +0530

    Updates

---
+++ precache-manifest.efc14cd593ca8035753b2a45e05f42d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b47c712c7d1484dc45dc7b42e602d5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3edb5a2d35946716c376",
+    "url": "/portfolio/static/css/main.fbbf2a35.chunk.css"
+  },
+  {
+    "revision": "aafaa147c73a1430262d",
+    "url": "/portfolio/static/js/2.4fc4f063.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/portfolio/static/js/2.4fc4f063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3edb5a2d35946716c376",
+    "url": "/portfolio/static/js/main.32a5e83f.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  }
+]);
